1c8a4fbdef88069c8cc389facc9721e992b04e9f
scot <riftedkris@gmail.com>
2025-11-01 18:23:59 +0200

    GITHUB DEKSTOP ADD FILES
    Added files

---
+++ poop-and-stuff-template-1.21.1/src/main/java/com/poopstuff/PoopAndStuff.java
@@ -0,0 +1,18 @@
+package com.poopstuff;
+
+import com.poopstuff.item.ModItems;
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class PoopAndStuff implements ModInitializer {
+	public static final String MOD_ID = "poop-and-stuff";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+        ModItems.registerModItems();
+		LOGGER.info("THIS MOD IS IN BETA");
+	}
+}
---
+++ poop-and-stuff-template-1.21.1/src/main/java/com/poopstuff/PoopAndStuffClient.java
@@ -0,0 +1,10 @@
+package com.poopstuff;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class PoopAndStuffClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
